540427c3e2f7185aaf3787e5bb6bf8dee0d54877
Si4oen <58781004+si4oen@users.noreply.github.com>
2019-12-22 03:51:02 +0700

    updated

--- vagrantfiles/bootstrap.sh
+++ vagrantfiles/bootstrap.sh
@@ -2,28 +2,28 @@
 
 ## Update the system
 echo "[TASK] Updating the system"
-yum install -y epel-release 2>&1
+yum install -y epel-release >/dev/null 2>&1
 yum update -y >/dev/null 2>&1
 
 ## Install desired packages
 echo "[TASK] Installing desired packages"
-yum install -y net-tools bind-utils  >/dev/null 2>&1
+yum install -y -q net-tools bind-utils  >/dev/null 2>&1
 
 ## Enable password authentication
-echo "[TASK] Enabling password authentication in sshd config"
+echo "[TASK] Enabled password authentication in sshd config"
 sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
 systemctl reload sshd
 
-## Disable password authentication
-echo "[TASK] Enabling password authentication in sshd config"
+## Disable Password authentication
+echo "[TASK] Disabled Password authentication in sshd config"
 sed -i -e "\\#PasswordAuthentication yes# s#PasswordAuthentication yes#PasswordAuthentication no#g" /etc/ssh/sshd_config
 systemctl reload sshd
 
-## Set Root password
+## Set Root Password
 echo "[TASK] Set root password"
 echo "centos" | passwd --stdin root >/dev/null 2>&1
 
-## Disable and stop firewalld
+## Disable and Stop firewalld
 echo "[TASK] Disable and stop firewalld"
 systemctl disable firewalld >/dev/null 2>&1
 systemctl stop firewalld
@@ -34,7 +34,7 @@ setenforce 0
 sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
 
 ## Update hosts file
-#echo "[TASK 6] Update /etc/hosts file"
+#echo "[TASK] Update /etc/hosts file"
 #cat >>/etc/hosts<<EOF
 #172.42.42.10 server.example.com server
 #172.42.42.20 client.example.com client
