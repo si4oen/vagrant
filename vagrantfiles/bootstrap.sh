209bef6fad9fee0e274196539495f0c81bd2a4fc
Si4oen <58781004+si4oen@users.noreply.github.com>
2019-12-22 03:54:31 +0700

    Update bootstrap.sh

--- vagrantfiles/bootstrap.sh
+++ vagrantfiles/bootstrap.sh
@@ -10,9 +10,9 @@ echo "[TASK] Installing desired packages"
 yum install -y -q net-tools bind-utils  >/dev/null 2>&1
 
 ## Enable password authentication
-echo "[TASK] Enabled password authentication in sshd config"
-sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
-systemctl reload sshd
+#echo "[TASK] Enabled password authentication in sshd config"
+#sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+#systemctl reload sshd
 
 ## Disable Password authentication
 echo "[TASK] Disabled Password authentication in sshd config"
@@ -34,7 +34,7 @@ setenforce 0
 sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
 
 ## Update hosts file
-#echo "[TASK] Update /etc/hosts file"
+#echo "[TASK] Update host file /etc/hosts"
 #cat >>/etc/hosts<<EOF
 #172.42.42.10 server.example.com server
 #172.42.42.20 client.example.com client
