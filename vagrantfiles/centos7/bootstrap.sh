3c160da392b90629e2ab8c4457e5817f922bb23a
Si4oen <58781004+si4oen@users.noreply.github.com>
2020-01-04 22:03:07 +0700

    Update bootstrap.sh

--- vagrantfiles/centos7/bootstrap.sh
+++ vagrantfiles/centos7/bootstrap.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 ## Update the system >/dev/null 2>&1
-echo "[TASK] Updating the system"
+echo "===== [TASK] Updating the system ====="
 yum install -y epel-release
 yum update -y
 
 ## Install desired packages
-echo "[TASK] Installing desired packages"
+echo "===== [TASK] Installing desired packages ====="
 yum install -y telnet htop net-tools wget nano
 
 ## Enable password authentication
-echo "[TASK] Enabled password authentication in sshd config"
+echo "===== [TASK] Enabled SSH password authentication ====="
 sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
 systemctl reload sshd
 
 ## Disable Password authentication
-#echo "[TASK] Disabled Password authentication in sshd config"
+#echo "===== [TASK] Disabled SSH password authentication ====="
 #sed -i -e "\\#PasswordAuthentication yes# s#PasswordAuthentication yes#PasswordAuthentication no#g" /etc/ssh/sshd_config
 #systemctl reload sshd
 
 ## Set Root Password
-echo "[TASK] Set root password"
+echo "===== [TASK] Set root password ====="
 echo "centos" | passwd --stdin root >/dev/null 2>&1
 
 ## Disable and Stop firewalld
-echo "[TASK] Disable and stop firewalld"
+echo "===== [TASK] Disable and stop firewalld ====="
 systemctl disable firewalld
 systemctl stop firewalld
 
 ## Disable SELinux
-echo "[TASK] Disable SELinux"
+echo "===== [TASK] Disable SELinux ====="
 setenforce 0
 sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
 
 ## Cleanup system >/dev/null 2>&1
-echo "[TASK] Cleanup system"
+echo "===== [TASK] Cleanup system ====="
 package-cleanup -y --oldkernels --count=1
 yum -y autoremove
 yum clean all
@@ -52,7 +52,7 @@ cat /dev/null > ~/.bash_history && history -c
 #EOF
 
 ## Rebooting Server
-echo "[TASK] Rebooting server"
+echo "===== [TASK] Rebooting server ====="
 echo ""
-echo "========== Finished =========="
+echo "########## Finished ##########"
 sudo reboot now
