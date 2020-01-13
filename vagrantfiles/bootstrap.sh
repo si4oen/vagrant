0c62515fd375af01f260ee1a690d4082ee96e725
Si4oen <si4oen@users.noreply.github.com>
2020-01-13 16:03:46 +0700

    Update bootstrap.sh

--- vagrantfiles/bootstrap.sh
+++ vagrantfiles/bootstrap.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 ## Update the system >/dev/null 2>&1
-echo "===== [TASK] Updating the system ====="
+echo ">>>>> [TASK] Updating the system"
 yum install -y epel-release >/dev/null 2>&1
 yum update -y >/dev/null 2>&1
 
 ## Install desired packages
-echo "===== [TASK] Installing desired packages ====="
+echo ">>>>> [TASK] Installing desired packages"
 yum install -y telnet htop net-tools wget nano >/dev/null 2>&1
 
 ## Enable password authentication
-echo "===== [TASK] Enabled SSH password authentication ====="
+echo ">>>>> [TASK] Enabled SSH password authentication"
 sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
 systemctl reload sshd
 
 ## Disable Password authentication
-#echo "===== [TASK] Disabled SSH password authentication ====="
+#echo ">>>>> [TASK] Disabled SSH password authentication"
 #sed -i -e "\\#PasswordAuthentication yes# s#PasswordAuthentication yes#PasswordAuthentication no#g" /etc/ssh/sshd_config
 #systemctl reload sshd
 
 ## Set Root Password
-echo "===== [TASK] Set root password ====="
+echo ">>>>> [TASK] Set root password"
 echo "centos" | passwd --stdin root >/dev/null 2>&1
 
 ## Disable and Stop firewalld
-echo "===== [TASK] Disable and stop firewalld ====="
+echo ">>>>> [TASK] Disable and stop firewalld"
 systemctl disable firewalld >/dev/null 2>&1
 systemctl stop firewalld
 
 ## Disable SELinux
-echo "===== [TASK] Disable SELinux ====="
+echo ">>>>> [TASK] Disable SELinux"
 setenforce 0
 sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
 
 ## Cleanup system >/dev/null 2>&1
-echo "===== [TASK] Cleanup system ====="
+echo ">>>>> [TASK] Cleanup system"
 package-cleanup -y --oldkernels --count=1
 yum -y autoremove
 yum clean all
@@ -52,7 +52,7 @@ cat /dev/null > ~/.bash_history && history -c
 #EOF
 
 ## Rebooting Server
-echo "===== [TASK] Rebooting server ====="
+echo ">>>>> [TASK] Rebooting server"
 echo ""
 echo "########## Finished ##########"
 sudo reboot now
