5964f310ebef8a8a0191876a60b32c4b8ddc45d1
Si4oen <nguyenthanhdanh@live.com>
2020-05-01 17:37:53 +0700

    Update bootstrap.sh

--- vagrantfiles/ubuntu18/bootstrap.sh
+++ vagrantfiles/ubuntu18/bootstrap.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-## Update the system
+## Set TimeZone to Asia/Ho_Chi_Minh
+echo ">>>>> [TASK] Set TimeZone to Asia/Ho_Chi_Minh"
+timedatectl set-timezone Asia/Ho_Chi_Minh
+
+## Update the system >/dev/null 2>&1
 echo ">>>>> [TASK] Updating the system"
 apt update >/dev/null 2>&1
 apt upgrade -y >/dev/null 2>&1
 
 ## Install desired packages
 echo ">>>>> [TASK] Installing desired packages"
-apt install -y net-tools wget unzip >/dev/null 2>&1
+apt install -y net-tools telnet htop wget unzip >/dev/null 2>&1
 
 ## Enable password authentication & SSH root login
 echo ">>>>> [TASK] Enabled password authentication in sshd config"
@@ -24,8 +28,24 @@ echo ">>>>> [TASK] Set root password"
 echo "root:ubuntu" | sudo chpasswd >/dev/null 2>&1
 
 ## Update hosts file
-#echo ">>>>> [TASK] Update host file /etc/hosts"
-#cat >>/etc/hosts<<EOF
-#172.42.42.10 server.example.com server
-#172.42.42.20 client.example.com client
-#EOF
+echo ">>>>> [TASK] Update host file /etc/hosts"
+cat >>/etc/hosts<<EOF
+192.168.16.161 gitlab1.testlab.local gitlab1
+192.168.16.151 docker1.testlab.local docker1
+192.168.16.141 jenkins1.testlab.local jenkins1
+192.168.16.130 kmaster.testlab.local kmaster
+192.168.16.131 kworker1.testlab.local kworker1
+192.168.16.132 kworker2.testlab.local kworker2
+192.168.16.133 microk8s.testlab.local microk8s
+EOF
+
+## Cleanup system >/dev/null 2>&1
+echo ">>>>> [TASK] Cleanup system"
+apt-get --purge autoremove -y >/dev/null 2>&1
+apt-get autoremove -y >/dev/null 2>&1
+apt-get clean >/dev/null 2>&1
+rm -rf /tmp/*
+rm -f /var/log/wtmp /var/log/btmp
+#dd if=/dev/zero of=/EMPTY bs=1M
+#rm -f /EMPTY
+cat /dev/null > ~/.bash_history && history -c
